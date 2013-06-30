23da0d913c8cded0fa5a1486af747e37f6c40cef
Yusaku ONO <yono.05@gmail.com>
2013-06-30 14:52:45 +0900

    install passenger gem

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# -*- coding:utf-8 -*-
+
+default["rbenv"]["global"] = "2.0.0-p247"
+default["passenger"]["version"] = "4.0.2"
+default["passenger"]["apache2"]["conf"] = "/etc/apache2/conf.d/passenger.conf"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,28 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "rbenv::system"
+rbenv_ruby node["rbenv"]["global"]
+rbenv_global node["rbenv"]["global"]
+
+rbenv_gem "passenger" do
+  version node["passenger"]["version"]
+end
+
+include_recipe "build-essential"
+%(libcurl4-openssl-dev apache2-prefork-dev libapr1-dev libaprutil1-dev).each do |package_name|
+  package package_name
+end
+
+rbenv_script "passenger_module" do
+  conf_path = node["passenger"]["apache2"]["conf"]
+  code <<-CODE
+    passenger-install-apache2-module --auto
+    rm -f #{conf_path}
+    echo LoadModule passenger_module `passenger-config --root`/libout/apache2/mod_passenger.so >> #{conf_path}
+    echo PassengerRoot `passenger-config --root` >> #{conf_path}
+    echo PassengerDefaultRuby `rbenv which ruby` >> #{conf_path}
+  CODE
+  not_if { File.exists?(conf_path) }
+end
